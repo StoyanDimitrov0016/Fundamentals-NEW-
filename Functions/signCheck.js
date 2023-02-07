4ad4b3eb96f92e8ac13b3f67686fb931361ba8db
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-02-07 19:56:47 +0200

    Uploading solved problems from Function - Exercise

--- Functions/signCheck.js
+++ Functions/signCheck.js
@@ -1,4 +1,5 @@
 function signCheck(num1, num2, num3) {
+    // In this task I am not allowed to use multiplication 
     let result = '';
     if ((num1 < 0 && num2 < 0) || (num1 > 0 && num2 > 0)) {
         if (num3 > 0) {
@@ -18,4 +19,4 @@ function signCheck(num1, num2, num3) {
     }
     console.log(result);
 }
-signCheck(5, 12, -15)
+signCheck(5, 12, -15);
