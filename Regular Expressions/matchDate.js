01867eddcce0ab7f78c97aa25350c84f210563c0
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-03-01 22:48:26 +0200

    Solved tastks from RexEx

---
+++ Regular Expressions/matchDate.js
@@ -0,0 +1,12 @@
+function findValidDates(datesList) {
+    const pattern = /(?<days>\d{2})(?<separator>[\.-/])(?<month>[A-Z][a-z]{2})\2(?<years>\d{4})/g
+    let match = pattern.exec(datesList);
+    while (match) {
+        const day = match.groups.days;
+        const month = match.groups.month;
+        const year = match.groups.years;
+        console.log(`Day: ${day}, Month: ${month}, Year: ${year}`);
+        match = pattern.exec(datesList);
+    }
+}
+findValidDates("13/Jul/1928, 10-Nov-1934, , 01/Jan-1951,f 25.Dec.1937 23/09/1973, 1/Feb/2016")
---
+++ Regular Expressions/matchFullName.js
@@ -0,0 +1,11 @@
+function findValidFullNames(nameList) {
+    let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/gm;
+    const validNames = [];
+    let match = pattern.exec(nameList)
+    while (match != null) {
+        validNames.push(match[0]);
+        match = pattern.exec(nameList);
+    }
+    console.log(validNames.join(' '));
+}
+findValidFullNames("Ivan Ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Test Testov, Ivan Ivanov")
