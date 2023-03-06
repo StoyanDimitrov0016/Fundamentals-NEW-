f8832094928502c088d2b1b676dedafff5b3d812
Stoyan Dimitrov <stoyan1020@gmail.com>
2023-03-06 18:27:58 +0200

    Improved version

--- Exam Preparation/secretChat.js
+++ Exam Preparation/secretChat.js
@@ -1,24 +1,33 @@
 function secretChat(input) {
     let initialLine = input.shift();
+
     while (input[0] != 'Reveal') {
+
         const command = input.shift()
         const [action, value1, value2] = command.split(':|:');
+
         switch (action) {
             case 'InsertSpace':
                 const indexForSpace = Number(value1);
+
                 initialLine = initialLine.split('');
                 initialLine.splice(indexForSpace, 0, ' ');
                 initialLine = initialLine.join('');
+
                 console.log(initialLine);
                 break;
             case 'Reverse':
+
                 if (initialLine.includes(value1)) {
+
                     let takenPart = initialLine.split('')
                         .splice(initialLine.indexOf(value1), value1.length)
                         .reverse()
                         .join('');
+
                     initialLine = initialLine.replace(value1, '');
                     initialLine = initialLine.concat(takenPart);
+
                     console.log(initialLine);
                 } else {
                     console.log('error');
@@ -27,15 +36,16 @@ function secretChat(input) {
             case 'ChangeAll':
                 const stringForReplacement = value1;
                 const replacementString = value2;
-                while (initialLine.includes(stringForReplacement)) {
-                    initialLine = initialLine.replace(stringForReplacement, replacementString);
-                }
+                const pattern = new RegExp(stringForReplacement, 'g')
+                initialLine = initialLine.replace(pattern, replacementString);
+
                 console.log(initialLine);
                 break;
         }
     }
     console.log('You have a new text message:', initialLine);
 }
+
 secretChat([
     'heVVodar!gniV',
     'ChangeAll:|:V:|:l',
